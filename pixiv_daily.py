1f138bcc02506dfa70789f8982ac53339bc449a9
chenjunyao <chen13157379235@outlook.com>
2018-10-08 19:24:59 +0800

    #add download func

--- pixiv_daily.py
+++ pixiv_daily.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import requests
 import re
+from io import BytesIO
+
+import requests
 import urllib3
+from PIL import Image
 
 
 class PixivDaily:
@@ -30,23 +33,34 @@ class PixivDaily:
     # prepare download params
     def fix(self, html):
         path = re.findall(self.pattern_url, html)
-        return [{
-                    'illust_id': re.findall(self.pattern_illust_id, p)[0],
-                    'referer': "http://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=" 
-                               + re.findall(self.pattern_illust_id, p)[0] + "&page=0",
-                    'url': p.replace('/c/240x480', '')
-                } for p in path]
+        path_ids = [dict(path=p,
+                         id=re.findall(self.pattern_illust_id, p)[0])
+                    for p in path]
+        return [dict(
+            illust_id=p['id'],
+            referer="http://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=" +
+                p['id'] + "&page=0",
+                short_name='illust_id_' + p['id'],
+                url=p['path'].replace('/c/240x480', '')
+                ) for p in path_ids]
 
-    def downloadPics(self, list):
-        pass
+    def downloadPics(self, params, local_path):
+        for param in params:
+            url = param['url']
+            referer = {'Referer': param['referer']}
+            save_path = local_path + '\\' + param['short_name']
+            res = requests.request('get', url, headers=referer)
+            img = Image.open(BytesIO(res.content))
+            img.save(save_path + '.jpg')
+            res.close()
 
-# main
-def run():
+
+def run(local_path):
     spider = PixivDaily()
     html = spider.getHtml(PixivDaily.requestUrl)
     res = spider.fix(html)
-    pass
+    spider.downloadPics(res, local_path)
 
 
 if __name__ == "__main__":
-    run()
+    run('E:\\FArtorias\\Pics')
